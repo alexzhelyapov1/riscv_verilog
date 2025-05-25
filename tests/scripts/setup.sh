96e8f854a05a8649f3487dcec74a1dd58a36eabc
Zhelyapov Aleksey <alexzhelyapov1@mail.ru>
2025-05-25 13:44:29 +0300

    Init

---
+++ tests/scripts/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd $(dirname "$0")
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo "This script must be run as root: sudo $0"
+    exit 1
+fi
+
+if ! command -v lsb_release >/dev/null || [ "$(lsb_release -is)" != "Ubuntu" ]; then
+    echo "[ERROR] This script for Ubuntu OS only."
+    exit 1
+fi
+
+sudo apt update
+sudo apt upgrade
+sudo apt install verilator
