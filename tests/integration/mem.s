50bf26f4160be86562bb7295c64121ad9d48921a
Zhelyapov Aleksey <zhelyapov.aleksey@huawei-partners.com>
2025-05-28 21:18:48 +0300

    Integration tests passed (no complex)

---
+++ tests/integration/mem.s
@@ -0,0 +1,24 @@
+.section .text
+.global _start
+
+_start:
+    addi x1, x0, 7
+    sd x1, 0x30(x0)
+    ld x2, 0x30(x0)
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
