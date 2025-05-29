29c512cadfd2d90c4e0ee1be393be4a29e7e6323
Zhelyapov Aleksey <alexzhelyapov1@mail.ru>
2025-05-29 12:43:12 +0300

    Otl

--- rtl/core/instruction_memory.sv
+++ rtl/core/instruction_memory.sv
@@ -1,4 +1,3 @@
-// rtl/core/instruction_memory.sv
 `include "common/defines.svh"
 
 module instruction_memory (
@@ -7,8 +6,11 @@ module instruction_memory (
 );
 
     parameter string INSTR_MEM_INIT_FILE_PARAM = ""; // Parameter for memory initialization file
-    localparam ROM_SIZE = 32'h100000; // Number of instructions
+    localparam ROM_SIZE = 2**20; // Number of instructions (1,048,576)
+    localparam ROM_ADDR_WIDTH = $clog2(ROM_SIZE); // Width of the index for mem (20 bits)
+
     logic [`INSTR_WIDTH-1:0] mem[ROM_SIZE-1:0];
+    logic [ROM_ADDR_WIDTH-1:0] mem_idx;
 
     initial begin
         // Default initialize all memory to NOP
@@ -17,22 +19,18 @@ module instruction_memory (
         end
 
         if (INSTR_MEM_INIT_FILE_PARAM != "") begin
-            // If a file is specified, load it. This will override NOPs.
             $readmemh(INSTR_MEM_INIT_FILE_PARAM, mem);
-            // $display("Instruction memory initialized from %s", INSTR_MEM_INIT_FILE_PARAM);
         end else begin
-            // Fallback to hardcoded test instructions if no file is provided
-            // These will override the NOPs at specific locations.
             mem[0] = 32'h00100093; // addi x1, x0, 1
             mem[1] = 32'h00200113; // addi x2, x0, 2
             mem[2] = 32'h00308193; // addi x3, x1, 3
             mem[3] = 32'h00110213; // addi x4, x2, 1
-            // $display("Instruction memory initialized with default test program.");
         end
     end
 
-    assign instruction = (address[`DATA_WIDTH-1:2] < ROM_SIZE) ?
-                     mem[address[`DATA_WIDTH-1:2]] :
-                     `NOP_INSTRUCTION; // Return NOP for out-of-bounds access
+    assign mem_idx = address[ROM_ADDR_WIDTH+2-1:2];
+    assign instruction = (mem_idx < ROM_SIZE) ?
+                         mem[mem_idx] :
+                         `NOP_INSTRUCTION;
 
 endmodule
